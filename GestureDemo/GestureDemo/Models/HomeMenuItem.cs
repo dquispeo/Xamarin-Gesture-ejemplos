f7170fd37f9458ac66465871a0fc09789430b0e5
DANIEL <daniel.quispe.o@tecsup.edu.pe>
2020-05-26 23:10:53 -0500

    first commit

---
+++ GestureDemo/GestureDemo/Models/HomeMenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestureDemo.Models
+{
+    public enum MenuItemType
+
+    {
+        Browse,
+        About,
+        TapDemo,
+        PinchDemo,
+        PanDemo,
+        SwipeDemo,
+        SwipeBindingDemo,
+
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
